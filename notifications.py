3c2e0bf1a4e8b3dbefafbb324a40408bdc2e4bcf
stevokollar463-lab <stevokollar463@gmail.com>
2026-07-21 14:31:20 +0200

    Create notifications.py

---
+++ notifications.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from pushbullet import Pushbullet
+from config import PB_API_KEY
+
+
+def posli_upozornenie(text):
+    try:
+        if PB_API_KEY:
+            pb = Pushbullet(PB_API_KEY)
+            pb.push_note("🎸 NOVÝ DOPYT", text)
+            return True
+    except Exception as e:
+        st.error(f"⚠️ Pushbullet neodoslal správu! Chyba: {e}")
+    return False
